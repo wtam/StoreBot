21e24e0d55bdc61bdfbf0b216eb22c138eb7036d
wtam <william.tam.2013@hotmail.com.hk>
2017-04-19 12:37:34 +0800

    clean up hidden char

--- provider.js
+++ provider.js
@@ -1,46 +1,46 @@
-"use strict";
-const handoff_1 = require("./handoff");
+"use strict"
+const handoff_1 = require("./handoff")
 exports.init = () => {
-    exports.conversations = [];
-};
+    exports.conversations = []
+}
 // Update
 const addToTranscript = (by, text) => {
-    const conversation = getConversation(by);
+    const conversation = getConversation(by)
     if (!conversation)
-        return false;
+        return false
     conversation.transcript.push({
         timestamp: Date.now(),
         from: by.agentConversationId ? 'agent' : 'customer',
         text
-    });
-    return true;
-};
+    })
+    return true
+}
 const connectCustomerToAgent = (by, agentAddress) => {
-    const conversation = getConversation(by);
+    const conversation = getConversation(by)
     if (conversation) {
-        conversation.state = handoff_1.ConversationState.Agent;
-        conversation.agent = agentAddress;
+        conversation.state = handoff_1.ConversationState.Agent
+        conversation.agent = agentAddress
     }
-    return conversation;
-};
+    return conversation
+}
 const queueCustomerForAgent = (by) => {
-    const conversation = getConversation(by);
+    const conversation = getConversation(by)
     if (!conversation)
-        return false;
-    conversation.state = handoff_1.ConversationState.Waiting;
+        return false
+    conversation.state = handoff_1.ConversationState.Waiting
     if (conversation.agent)
-        delete conversation.agent;
-    return true;
-};
+        delete conversation.agent
+    return true
+}
 const connectCustomerToBot = (by) => {
-    const conversation = getConversation(by);
+    const conversation = getConversation(by)
     if (!conversation)
-        return false;
-    conversation.state = handoff_1.ConversationState.Bot;
+        return false
+    conversation.state = handoff_1.ConversationState.Bot
     if (conversation.agent)
-        delete conversation.agent;
-    return true;
-};
+        delete conversation.agent
+    return true
+}
 // Get
 // if looking up by customerConversationId, create new conversation if one doesn't already exist
 const getConversation = (by, customerAddress ) => {
@@ -50,32 +50,32 @@ const getConversation = (by, customerAddress ) => {
             customer: customerAddress,
             state: handoff_1.ConversationState.Bot,
             transcript: []
-        };
-        exports.conversations.push(conversation);
-        return conversation;
-    };
+        }
+        exports.conversations.push(conversation)
+        return conversation
+    }
     if (by.bestChoice) {
         const waitingLongest = exports.conversations
             .filter(conversation => conversation.state === handoff_1.ConversationState.Waiting)
-            .sort((x, y) => y.transcript[y.transcript.length - 1].timestamp - x.transcript[x.transcript.length - 1].timestamp);
-        return waitingLongest.length > 0 && waitingLongest[0];
+            .sort((x, y) => y.transcript[y.transcript.length - 1].timestamp - x.transcript[x.transcript.length - 1].timestamp)
+        return waitingLongest.length > 0 && waitingLongest[0]
     }
     if (by.customerName) {
-        return exports.conversations.find(conversation => conversation.customer.user.name == by.customerName);
+        return exports.conversations.find(conversation => conversation.customer.user.name == by.customerName)
     }
     else if (by.agentConversationId) {
-        return exports.conversations.find(conversation => conversation.agent && conversation.agent.conversation.id === by.agentConversationId);
+        return exports.conversations.find(conversation => conversation.agent && conversation.agent.conversation.id === by.agentConversationId)
     }
     else if (by.customerConversationId) {
-        let conversation = exports.conversations.find(conversation => conversation.customer.conversation.id === by.customerConversationId);
+        let conversation = exports.conversations.find(conversation => conversation.customer.conversation.id === by.customerConversationId)
         if (!conversation && customerAddress) {
-            conversation = createConversation(customerAddress);
+            conversation = createConversation(customerAddress)
         }
-        return conversation;
+        return conversation
     }
-    return null;
-};
-const currentConversations = () => exports.conversations;
+    return null
+}
+const currentConversations = () => exports.conversations
 exports.defaultProvider = {
     init: exports.init,
     // Update
@@ -86,4 +86,4 @@ exports.defaultProvider = {
     // Get
     getConversation,
     currentConversations,
-};
+}
